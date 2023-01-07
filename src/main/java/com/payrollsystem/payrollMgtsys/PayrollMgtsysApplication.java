018e986be211023eedcb5099e83262774cadc8a5
sbj276 <sbj276@gmail.com>
2023-01-07 14:30:18 +0530

    project setup

---
+++ src/main/java/com/payrollsystem/payrollMgtsys/PayrollMgtsysApplication.java
@@ -0,0 +1,13 @@
+package com.payrollsystem.payrollMgtsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollMgtsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollMgtsysApplication.class, args);
+	}
+
+}
